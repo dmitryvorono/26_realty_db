1557eab49c68898479bcd0b5b9e7d0a7e07de36b
Dmitry <voronov-d88@yandex.ru>
2017-11-22 11:54:25 +0000

    add filters again

--- app/views.py
+++ app/views.py
@@ -1,26 +1,26 @@
-from app import app
+from app import app, db, models
+from flask import render_template
 from flask import request
-from sqlalchemy import create_engine
-import config
-from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from sqlalchemy import or_
-from flask import render_template
 
 
 @app.route('/')
 def ads_list():
-    return render_template('ads_list.html', ads=[{
-            "settlement": "Череповец",
-            "under_construction": False,
-            "description": '''Квартира в отличном состоянии. Заезжай и живи!''',
-            "price": 2080000,
-            "oblast_district": "Череповецкий район",
-            "living_area": 17.3,
-            "has_balcony": True,
-            "address": "Юбилейная",
-            "construction_year": 2001,
-            "rooms_number": 2,
-            "premise_area": 43.0,
-        }]*10
-    )
+    city = request.args.get('oblast_district', default='', type=str)
+    is_new_building_only = request.args.get('new_building',
+                                            default=False, type=bool)
+    min_price = request.args.get('min_price', default=0, type=int)
+    max_price = request.args.get('max_price', default=0, type=int)
+    ads = db.session.query(models.Advertisement)
+    if city:
+        ads = ads.filter(models.Advertisement.settlement == city)
+    if min_price:
+        ads = ads.filter(models.Advertisement.price >= min_price)
+    if max_price:
+        ads = ads.filter(models.Advertisement.price <= max_price)
+    if is_new_building_only:
+        now = datetime.now()
+        ads = ads.filter(or_(models.Advertisement.under_construction.is_(True),
+                             models.Advertisement.construction_year >= now.year - 3))
+    return render_template('ads_list.html', ads=ads)
