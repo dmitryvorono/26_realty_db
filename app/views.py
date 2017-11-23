2e4428377ccb3fbad19f3b2f1f36177ad66ee91e
Dmitry <voronov-d88@yandex.ru>
2017-11-23 08:06:47 +0000

    add pagination

--- app/views.py
+++ app/views.py
@@ -3,10 +3,12 @@ from flask import render_template
 from flask import request
 from datetime import datetime
 from sqlalchemy import or_
+from config import ads_per_page
 
 
-@app.route('/')
-def ads_list():
+@app.route('/', methods=['GET', 'POST'], defaults={'page': 1})
+@app.route('/<int:page>', methods=['GET', 'POST'])
+def ads_list(page):
     city = request.args.get('oblast_district', default='', type=str)
     is_new_building_only = request.args.get('new_building',
                                             default=False, type=bool)
@@ -23,4 +25,7 @@ def ads_list():
         now = datetime.now()
         ads = ads.filter(or_(models.Advertisement.under_construction.is_(True),
                              models.Advertisement.construction_year >= now.year - 3))
-    return render_template('ads_list.html', ads=ads)
+    paginate_ads = ads.paginate(page, ads_per_page, False)
+    return render_template('ads_list.html',
+                           ads=paginate_ads.items,
+                           pagination=paginate_ads)
